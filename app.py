2420620f8a0ad0b4bc6623dc50a841b762f9d2fd
felix44555 <felix.lessig@gmail.com>
2026-06-09 11:26:06 +0200

    fff

--- app.py
+++ app.py
@@ -1738,7 +1738,7 @@ def main() -> None:
     # -------------------------------------------------------------------------
     # Tabellen
     # -------------------------------------------------------------------------
-    with st.expander("Stündliche Tabelle"):
+    with st.expander("Stündliche Tabelle"):sss
         st.dataframe(
             df[[
                 "Stunde", "Last_GW", "PV_GW", "Wind_GW",
